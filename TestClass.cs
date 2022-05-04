21d8e2f962f7b5d909fcac37904ecdb2ef5e48d2
Sanela Cvijic <sanela26@gmail.com>
2022-05-04 13:00:40 +0200

    Add project files.

---
+++ TestClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using EC = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Pract31
+{
+    internal class TestClass
+    {
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [Test] 
+        public void TestGoogleSearch()
+        {
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+            }
+    }
+}
